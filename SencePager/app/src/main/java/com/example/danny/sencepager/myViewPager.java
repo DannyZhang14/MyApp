c3e89617c837dc83b70bdeb201034822940f408d
DannyZhang14 <danny.zhang@lumiunited.com>
2015-08-13 20:06:21 +0800

    3layout file

--- SencePager/app/src/main/java/com/example/danny/sencepager/myViewPager.java
+++ SencePager/app/src/main/java/com/example/danny/sencepager/myViewPager.java
@@ -1,6 +1,8 @@
 package com.example.danny.sencepager;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -66,14 +68,51 @@ public class myViewPager extends Activity {
     }
     public class MyPagerAdapter extends PagerAdapter{
         public  List<View> mListViews;
+
         public MyPagerAdapter(List<View> mListViews){
             this.mListViews = mListViews;
         }
         @Override
-        public  void destoryItem(View arg0,int arg1,Objects args){
+        public  void destoryItem(View arg0,int arg1,Objects arg2){
+            ((ViewPager) arg0).removeView(mListViews.get(arg1));
+
+        }
+        @Override
+        public  void finishUpdate(View arg0){
+
+        }
+        @Override
+        public int getCount(){
+            return mListViews.size();
+        }
+        @Override
+        public  Object instantiateItem(View arg0,int arg1){
+            ((ViewPager)arg0).addView(mListViews.get(arg1),0);
+            return mListViews.get(arg1);
+        }
+        @Override
+        public  boolean isViewFromObject(View arg0,Object arg1){
+            return arg0 ==(arg1);
+        }
+        @Override
+        public  void restoreState(Parcelable arg0,ClassLoader arg1){
+
+        }
+        @Override
+        public  Parcelable saveState(){
+            return null;
+        }
+        @Override
+        public void startUpdate(View arg0){
 
         }
     }
 
+
+    private void InitImageView(){
+        cursor =(ImageView) findViewById(R.id.cursor);
+        bmpW = Bitmap
+
+    }
 
 }
