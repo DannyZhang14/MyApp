ef66bc88098c9a0a017dea139a146bf5a7253605
dannyzhang14 <danny.zhang@lumiunited.com>
2015-09-02 19:44:09 +0800

    "add ViewPager"

--- SencePager/app/src/main/java/com/example/danny/sencepager/myViewPager.java
+++ SencePager/app/src/main/java/com/example/danny/sencepager/myViewPager.java
@@ -2,15 +2,19 @@ package com.example.danny.sencepager;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.os.Parcelable;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -21,106 +25,118 @@ import java.util.List;
 import java.util.Objects;
 
 public class myViewPager extends Activity {
-
-    private ViewPager mPager;
-    private List<View> listViews;
-    private ImageView cursor;
-    private int offset = 0;
-    private int currIndex = 0;
-    private int bmpW;
-    private ImageButton mImageButton;
+    private ViewPager viewPager;
+    private ImageView imageView;
+    private List<View> lists = new ArrayList<View>();
+    private pagerAdapter myAdapter;
+    private Bitmap cursor;
+    private int offSet;
+    private int currentItem;
+    private Matrix matrix = new Matrix();
+    private int bmWidth;
+    private Animation animation;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view_pager);
 
+        imageView = (ImageView) findViewById(R.id.cursor);
+        textView1 = (TextView) findViewById(R.id.Pager1);
+        textView2 = (TextView) findViewById(R.id.Pager2);
+        textView3 = (TextView) findViewById(R.id.Pager3);
 
+        lists.add(getLayoutInflater().inflate(R.layout.layout1,null));
+        lists.add(getLayoutInflater().inflate(R.layout.layout2,null));
+        lists.add(getLayoutInflater().inflate(R.layout.layout3, null));
 
-        mImageButton = (ImageButton)findViewById(R.id.buttonImage);
-        mImageButton.setOnClickListener(new View.OnClickListener() {
+        //initeCursor();
+        myAdapter = new pagerAdapter(lists);
+
+        viewPager = (ViewPager) findViewById(R.id.vPager);
+        viewPager.setAdapter(myAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        if (currentItem == 1) {
+                            animation = new TranslateAnimation(offSet * 2 + bmWidth, 0, 0, 0);
+                        } else if (currentItem == 2) {
+                            animation = new TranslateAnimation(offSet * 4 + 2 * bmWidth, 0, 0, 0);
+                        }
+                        break;
+                    case 1:
+                        if (currentItem == 0) {
+                            animation = new TranslateAnimation(0, offSet * 2 + bmWidth, 0, 0);
+                        } else if (currentItem == 2) {
+                            animation = new TranslateAnimation(4 * offSet + 2 * bmWidth, offSet * 2 + bmWidth, 0, 0);
+                        }
+                        break;
+                    case 2:
+                        if (currentItem == 0) {
+                            animation = new TranslateAnimation(0, 4 * offSet + 2 * bmWidth, 0, 0);
+                        } else if (currentItem == 0) {
+                            animation = new TranslateAnimation(offSet * 2 + bmWidth, 4 * offSet + 2 * bmWidth, 0, 0);
+                        }
+                }
+                currentItem = position;
+                animation.setDuration(500);
+                animation.setFillAfter(true);
+                imageView.startAnimation(animation);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        textView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                viewPager.setCurrentItem(0);
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        });
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(2);
             }
         });
     }
-    /*
-    private void InitTextView(){
-        TextView t1 = (TextView) findViewById(R.id.Pager1);
-        TextView t2 = (TextView) findViewById(R.id.Pager2);
-        TextView t3 = (TextView) findViewById(R.id.Pager3);
 
-        t1.setOnClickListener(new MyOnClickListener(0));
-    }
-    public class MyOnClickListener implements View.OnClickListener{
-        private  int index =0;
-
-        public  MyOnClickListener(int i){
-            index = i;
-        }
-        @Override
-        public  void onClick(View v){
-            mPager.setCurrentItem(index);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      //  return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_my_view_pager,menu);
+        return true;
     }
 
-/
-    private  void InitViewPager(){
-        mPager = (ViewPager) findViewById(R.id.vPager);
-        listViews = new ArrayList<View>();
-        LayoutInflater mInflater = getLayoutInflater();
-        listViews.add(mInflater.inflate(R.layout.layout1,null));
-        listViews.add(mInflater.inflate(R.layout.layout2,null));
-        listViews.add(mInflater.inflate(R.layout.layout3,null));
-        mPager.setAdapter(new MyPagerAdapter(listViews));
-        mPager.setCurrentItem(0);
-        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
-
-    }
-    public class MyPagerAdapter extends PagerAdapter{
-        public  List<View> mListViews;
-
-        public MyPagerAdapter(List<View> mListViews){
-            this.mListViews = mListViews;
-        }
-        @Override
-        public  void destoryItem(View arg0,int arg1,Objects arg2){
-            ((ViewPager) arg0).removeView(mListViews.get(arg1));
-
-        }
-        @Override
-        public  void finishUpdate(View arg0){
-
-        }
-        @Override
-        public int getCount(){
-            return mListViews.size();
-        }
-        @Override
-        public  Object instantiateItem(View arg0,int arg1){
-            ((ViewPager)arg0).addView(mListViews.get(arg1),0);
-            return mListViews.get(arg1);
-        }
-        @Override
-        public  boolean isViewFromObject(View arg0,Object arg1){
-            return arg0 ==(arg1);
-        }
-        @Override
-        public  void restoreState(Parcelable arg0,ClassLoader arg1){
-
-        }
-        @Override
-        public  Parcelable saveState(){
-            return null;
-        }
-        @Override
-        public void startUpdate(View arg0){
-
-        }
-    }
-
-
-
-*/
+//    private void initeCursor(){
+//        cursor = BitmapFactory.decodeResource(getResources(),R.drawable.cu);
+//        bmWidth = cursor.getWidth();
+//        DisplayMetrics dm ;
+//        dm = getResources().getDisplayMetrics();
+//        offSet = (dm.widthPixels - 3 * bmWidth) /6;
+//        matrix.setTranslate(offSet, 0);
+//        imageView.setImageMatrix(matrix);
+//        currentItem = 0;
+//    }
 }
