21ec3dfd47ab0e05db9857757f18faf8e32fac09
dannyzhang14 <danny.zhang@lumiunited.com>
2015-09-17 21:06:12 +0800

    WebView添加

--- SencePager/app/src/main/java/com/example/danny/sencepager/mybaidu.java
+++ SencePager/app/src/main/java/com/example/danny/sencepager/mybaidu.java
@@ -4,21 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class mybaidu extends Activity {
     private Button mButton;
-    private EditText editText;
+    private TextView editText;
     private String mUrlAddress;
+    private String test="tesdddddddddddddddddddddddddddddddddddt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.baiduyixia);
-        editText = (EditText) findViewById(R.id.addresscontent);
-        mUrlAddress=editText.getText().toString();
+
+
 
 
         //点击事件的方法
@@ -26,10 +29,15 @@ public class mybaidu extends Activity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                editText = (TextView) findViewById(R.id.addresscontent);
+                mUrlAddress=editText.getText().toString();
                 Intent intent = new Intent();
                 intent.setClass(mybaidu.this,mywebview.class);
-                intent.putExtra("searchurl",mUrlAddress);
+                intent.putExtra("searchurl", mUrlAddress);
+
                 startActivity(intent);
+                Log.i("test",test);
+                Log.i("tag",mUrlAddress);
 
 
             }
